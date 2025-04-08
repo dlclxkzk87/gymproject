37f0f1c8e33d65425bcffb3a721b3f6feb11ddcd
SijinSeong <checkky517@gmail.com>
2025-04-08 16:10:17 +0900

    #36 [feat] loginview reupload

--- gymproject/src/loginview/LoginAdmin.java
+++ gymproject/src/loginview/LoginAdmin.java
@@ -3,47 +3,62 @@ package loginview;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 import db.ConnectionProvider;
 import adminview.AdminView; 
 
 public class LoginAdmin {
-    public void loginAdmin() {
+    public static void loginAdmin() {
         int id;
         String pwd;
         Scanner sc = new Scanner(System.in);
-        System.out.print("관리자 아이디를 입력하세요==>");
-        id = sc.nextInt();
-        System.out.print("관리자 비밀번호를 입력하세요==>");
-        pwd = sc.next();
-
-        String sql = "select * from admin where a_id=?";
-        try {
-            Connection conn = ConnectionProvider.getConnection();
-            PreparedStatement pstmt = conn.prepareStatement(sql);
-
-            pstmt.setInt(1, id);
-            ResultSet rs = pstmt.executeQuery();
-
-            if (rs.next()) {
-                if (pwd.equals(rs.getString(2))) {
-                    pstmt.close();
-                    conn.close();
-                    AdminView.showAdminMenu(); 
-                } else {
-                    System.out.println("비밀번호가 틀렸습니다\n");
-                    pstmt.close();
-                    conn.close();
-                }
-            } else {
-                System.out.println("존재하지 않는 관리자 아이디입니다\n");
-                pstmt.close();
-                conn.close();
-            }
-
-        } catch (Exception e) {
-            System.out.println("예외발생: " + e.getMessage());
+        while(true) {
+        	System.out.println();
+	        System.out.print("관리자 아이디를 입력하세요(뒤로 가려면 0을 입력하세요)==> ");
+	        try {
+				id = sc.nextInt();
+			}catch(InputMismatchException e) {
+				id = -1;
+				sc = new Scanner(System.in);
+			}
+	        if(id == 0) {
+				break;
+			}
+	        System.out.print("관리자 비밀번호를 입력하세요(뒤로 가려면 0을 입력하세요)==> ");
+	        pwd = sc.next();
+	        if(pwd.equals("0")) {
+				break;
+			}
+	
+	        String sql = "select * from admin where a_id=?";
+	        try {
+	            Connection conn = ConnectionProvider.getConnection();
+	            PreparedStatement pstmt = conn.prepareStatement(sql);
+	
+	            pstmt.setInt(1, id);
+	            ResultSet rs = pstmt.executeQuery();
+	
+	            if (rs.next()) {
+	                if (pwd.equals(rs.getString(2))) {
+	                    pstmt.close();
+	                    conn.close();
+	                    AdminView.showAdminMenu(); 
+	                } else {
+	                    System.out.println("비밀번호가 틀렸습니다");
+	                    pstmt.close();
+	                    conn.close();
+	                }
+	            } else {
+	                System.out.println("존재하지 않는 관리자 아이디입니다");
+	                pstmt.close();
+	                conn.close();
+	            }
+	
+	        } catch (Exception e) {
+	            System.out.println("예외발생: " + e.getMessage());
+	        }
         }
     }
 }
--- gymproject/src/loginview/LoginMember.java
+++ gymproject/src/loginview/LoginMember.java
@@ -3,6 +3,7 @@ package loginview;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 import db.ConnectionProvider;
 import memberview.MemberView;
@@ -10,42 +11,57 @@ import memberview.MemberView;
 public class LoginMember {
 	public static int loginId = -1;
 
-	public void loginMember() {
+	public static void loginMember() {
 		int id;
 		String pwd;
 		Scanner sc = new Scanner(System.in);
-		System.out.print("회원 아이디를 입력하세요==>");
-		id = sc.nextInt();
-		System.out.print("회원 비밀번호를 입력하세요==>");
-		pwd = sc.next();
-		String sql= "select * from member where m_id=?";
-		try {
-			Connection conn = ConnectionProvider.getConnection();
-			PreparedStatement pstmt = conn.prepareStatement(sql);
-			pstmt.setInt(1, id);
-			ResultSet rs = pstmt.executeQuery();
-
-			if(rs.next()) {
-				if(pwd.equals(rs.getString("m_pwd"))) {
-					loginId = id; 
-					System.out.println("로그인 성공!");
-					pstmt.close();
-					conn.close();
-					
-					// 로그인 성공 후 회원 메뉴로 이동
-					MemberView.showMemberMenu();
+		while(true) {
+			System.out.println();
+			System.out.print("회원 아이디를 입력하세요(뒤로 가려면 0을 입력하세요)==> ");
+			try {
+				id = sc.nextInt();
+			}catch(InputMismatchException e) {
+				id = -1;
+				sc = new Scanner(System.in);
+			}
+			if(id == 0) {
+				break;
+			}
+			System.out.print("회원 비밀번호를 입력하세요(뒤로 가려면 0을 입력하세요)==> ");
+			pwd = sc.next();
+			if(pwd.equals("0")) {
+				break;
+			}
+			
+			String sql= "select * from member where m_id=?";
+			try {
+				Connection conn = ConnectionProvider.getConnection();
+				PreparedStatement pstmt = conn.prepareStatement(sql);
+				pstmt.setInt(1, id);
+				ResultSet rs = pstmt.executeQuery();
+	
+				if(rs.next()) {
+					if(pwd.equals(rs.getString("m_pwd"))) {
+						loginId = id; 
+						System.out.println("로그인 성공!");
+						pstmt.close();
+						conn.close();
+						
+						// 로그인 성공 후 회원 메뉴로 이동
+						MemberView.showMemberMenu();
+					} else {
+						System.out.println("비밀번호가 틀렸습니다.");
+						pstmt.close();
+						conn.close();
+					}
 				} else {
-					System.out.println("비밀번호가 틀렸습니다.\n");
+					System.out.println("존재하지 않는 회원 아이디입니다.");
 					pstmt.close();
 					conn.close();
-				}
-			} else {
-				System.out.println("존재하지 않는 회원 아이디입니다.\n");
-				pstmt.close();
-				conn.close();
-			}					
-		}catch (Exception e) {
-			System.out.println("예외발생: " + e.getMessage());
+				}					
+			}catch (Exception e) {
+				System.out.println("예외발생: " + e.getMessage());
+			}
 		}
 	}
 }
