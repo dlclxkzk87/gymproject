55f1b22c58a3a1b1c241cf6cd9c83fae321ed9d6
SeChang <ricril1033@gmail.com>
2025-04-08 11:37:55 +0900

    fix_adminview

--- gymproject/src/adminview/AdminView.java
+++ gymproject/src/adminview/AdminView.java
@@ -1,34 +1,36 @@
 package adminview;
 
 import java.util.Scanner;
+import startview.StartView;
 
 public class AdminView {
 
-    public static void showAdminMenu() {
-        Scanner sc = new Scanner(System.in);
-        int menu;
+	public static void showAdminMenu() {
+		Scanner sc = new Scanner(System.in);
+		int menu;
 
-        do {
-            System.out.println("\n===== 관리자 메뉴 =====");
-            System.out.println("1. 정보 조회");
-            System.out.println("2. 정보 수정");
-            System.out.println("3. 계정 삭제");
-            System.out.println("4. 회원 정보 및 결제 내역 조회");
-            System.out.println("0. 로그아웃");
-            System.out.print("선택 >> ");
-            menu = sc.nextInt();
+		do {
+			System.out.println("\n===== 관리자 메뉴 =====");
+			System.out.println("1. 정보 조회");
+			System.out.println("2. 정보 수정");
+			System.out.println("3. 계정 삭제");
+			System.out.println("4. 회원 정보 및 결제 내역 조회");
+			System.out.println("0. 로그아웃");
+			System.out.print("선택 >> ");
+			menu = sc.nextInt();
 
-            switch (menu) {
-                case 1 -> AdminSearchView.showAdminList();
-                case 2 -> AdminUpdateView.upDateAdmin();
-                case 3 -> AdminDeleteView.deleteAdmin();
-                case 4 -> MemberListView.showMemberInfoMenu();
-                case 0 -> {
-                	System.out.println("로그아웃합니다.");
-                	StartView.start();
-                }
-                default -> System.out.println("잘못된 입력입니다.");
-            }
-        } while (menu != 0);
-    }
+			switch (menu) {
+			case 1 -> AdminSearchView.showAdminList();
+			case 2 -> AdminUpdateView.upDateAdmin();
+			case 3 -> AdminDeleteView.deleteAdmin();
+			case 4 -> MemberListView.showMemberInfoMenu();
+			case 0 -> {
+				System.out.println("로그아웃합니다.");
+				StartView st = new StartView();
+				st.start();
+			}
+			default -> System.out.println("잘못된 입력입니다.");
+			}
+		} while (menu != 0);
+	}
 }
